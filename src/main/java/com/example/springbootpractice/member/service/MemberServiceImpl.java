df955d1971e2516fbb45ad82e69331e17ad8491c
movingjin <shdlehdwls@gmail.com>
2023-07-30 19:17:33 +0900

    feat: register 메서드에 @Transactional 어노테이션 추가

--- src/main/java/com/example/springbootpractice/member/service/MemberServiceImpl.java
+++ src/main/java/com/example/springbootpractice/member/service/MemberServiceImpl.java
@@ -89,6 +89,7 @@ public class MemberServiceImpl implements MemberService{
     }
 
     @Override
+    @Transactional
     public boolean register(LoginRequestDto request) throws Exception {
         try {
             Member member = Member.builder()
